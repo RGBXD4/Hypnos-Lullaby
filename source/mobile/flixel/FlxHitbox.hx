f09568ad3e11ed397e2a574480f097a6434d4feb
8bit <139103755+RGBXD4@users.noreply.github.com>
2025-04-06 12:07:06 +0800

    Update FlxHitbox.hx

--- source/mobile/flixel/FlxHitbox.hx
+++ source/mobile/flixel/FlxHitbox.hx
@@ -30,9 +30,9 @@ class FlxHitbox extends FlxSpriteGroup
 			for (i in 0...5)
 			//这一段我不会写呃，所以我用ai给我跑了个代码不知道能不能用...
 			if (i ＜ 4)
-			add(buttons[i] = createHint(i * width, 0, width, height, colorsS[i]));
+			{add(buttons[i] = createHint(i * width, 0, width, height, colorsS[i]));}
 			else
-			add(buttons[i] = createHint(0, height, FlxG.width, widthS, colorsS[i]));
+			{add(buttons[i] = createHint(0, height, FlxG.width, widthS, colorsS[i]));}
 		}
 
 		scrollFactor.set();
