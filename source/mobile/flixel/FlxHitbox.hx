f4865ef0f1272935129fbb5dbaa3e886e6654842
8bit <139103755+RGBXD4@users.noreply.github.com>
2025-04-12 12:26:21 +0800

    Update FlxHitbox.hx

--- source/mobile/flixel/FlxHitbox.hx
+++ source/mobile/flixel/FlxHitbox.hx
@@ -10,91 +10,84 @@ import flixel.tweens.FlxTween;
 
 class FlxHitbox extends FlxSpriteGroup
 {
-    public var buttons:Array<FlxButton> = [];
-    
-    public function new(mode:Modes)
-    {
-        super();
+	public var buttons:Array<FlxButton> = [];
+	public var buttonS:FlxButton = new FlxButton(0, 0);
 
-        var width = Std.int(FlxG.width / 4);
-        var widthS = Std.int(FlxG.width / 5);
-        var height = Std.int(FlxG.height * 0.8);
-        var colors = [0xFF00FF, 0x00FFFF, 0x00FF00, 0xFF0000, 0xFFFF00]; // 添加第5个颜色
+	public function new(modes:Modes)
+	{
+		super();
 
-        switch(mode)
-        {
-            case DEFAULT:
-                for (i in 0...4) {
-                    buttons[i] = createHint(i * width, 0, width, FlxG.height, colors[i]);
-                    add(buttons[i]);
-                }
+		var width = Std.int(FlxG.width / 4);
+		var widthS = Std.int(FlxG.width / 5);
+		var height = Std.int(FlxG.height * 0.8);
+		var colors = [0xFF00FF, 0x00FFFF, 0x00FF00, 0xFF0000];
+		switch(modes)
+		{
+			case DEFAULT:
+			for (i in 0...4)
+			add(buttons[i] = createHint(i * width, 0, width, FlxG.height, colors[i]));
+			case SPACE:
+			for (i in 0...4)
+		    {
+			add(buttons[i] = createHint(i * width, 0, width, height, colors[i]));
+            }
+			add(buttonS = createHint(0, height, FlxG.width, widthS, 0xFFFF00));
+		}
 
-            case SPACE:
-                for (i in 0...5) {
-                    if (i < 4) {
-                        buttons[i] = createHint(i * width, 0, width, height, colors[i]);
-                    } else {
-                        buttons[i] = createHint(0, height, FlxG.width, FlxG.height - height, colors[i]);
-                    }
-                    add(buttons[i]);
-                }
-        }
+		scrollFactor.set();
+	}
 
-        scrollFactor.set();
-    }
+	override function destroy()
+	{
+		super.destroy();
+		buttonS = FlxDestroyUtil.destroy(buttonS);
+		for (button in buttons)
+			button = FlxDestroyUtil.destroy(button);
+	}
 
-    override function destroy()
-    {
-        for (i in 0...buttons.length) {
-            buttons[i] = FlxDestroyUtil.destroy(buttons[i]);
-        }
-        buttons = [];
-        
-        super.destroy();
-    }
+	function createHintGraphic(width:Int, height:Int, color:Int):BitmapData
+	{
+		var shape = new Shape();
+		shape.graphics.beginFill(color);
+		shape.graphics.drawRect(0, 0, width, height);
+		shape.graphics.endFill();
 
-    function createHintGraphic(width:Int, height:Int, color:Int):BitmapData
-    {
-        var shape = new Shape();
-        shape.graphics.beginFill(color);
-        shape.graphics.drawRect(0, 0, width, height);
-        shape.graphics.endFill();
+		var bitmap = new BitmapData(width, height, true, 0);
+		bitmap.draw(shape);
+		return bitmap;
+	}
 
-        var bitmap = new BitmapData(width, height, true, 0);
-        bitmap.draw(shape);
-        return bitmap;
-    }
+	function createHint(x:Float, y:Float, width:Int, height:Int, color:Int):FlxButton
+	{
+		var hintTween:FlxTween = null;
+		
+		var hint = new FlxButton(x, y);
+		hint.loadGraphic(createHintGraphic(width, height, color));
+		hint.solid = hint.immovable = false;
+		hint.scrollFactor.set();
+		hint.alpha = 0.00001;
 
-    function createHint(x:Float, y:Float, width:Int, height:Int, color:Int):FlxButton
-    {
-        var hint = new FlxButton(x, y);
-        hint.loadGraphic(createHintGraphic(width, height, color));
-        hint.solid = false;
-        hint.immovable = true;
-        hint.scrollFactor.set();
-        hint.alpha = 0.00001;
+		hint.onDown.callback = hint.onOver.callback = () -> {
+			if (hintTween != null)
+				hintTween.cancel();
+			hint.alpha = 0.3;
+		};
 
-        var hintTween:FlxTween = null;
-        hint.onDown.callback = hint.onOver.callback = () -> {
-            if (hintTween != null) hintTween.cancel();
-            hint.alpha = 0.3;
-        };
+		hint.onUp.callback = hint.onOut.callback = () -> {
+			if (hintTween != null)
+				hintTween.cancel();
+			hintTween = FlxTween.tween(hint, { alpha: 0.00001 }, 0.2);
+		};
 
-        hint.onUp.callback = hint.onOut.callback = () -> {
-            if (hintTween != null) hintTween.cancel();
-            hintTween = FlxTween.tween(hint, {alpha: 0.00001}, 0.2);
-        };
+		#if FLX_DEBUG
+		hint.ignoreDrawDebug = true;
+		#end
 
-        #if FLX_DEBUG
-        hint.ignoreDrawDebug = true;
-        #end
-
-        return hint;
-    }
+		return hint;
+	}
 }
-
 enum Modes
 {
-    DEFAULT;
-    SPACE;
+DEFAULT;
+SPACE;
 }
